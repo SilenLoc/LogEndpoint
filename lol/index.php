06dcbaf2d6f6d5520704269e58921f3f16d8f5bb
silenlocatelli <halloduidiot>
2021-12-15 17:36:32 +0100

    added body

--- lol/index.php
+++ lol/index.php
@@ -7,15 +7,13 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Page Title</title>
-    <meta name="description" content="Webpage for xxxx">
-    <!-- http://meyerweb.com/eric/tools/css/reset/ -->
-    <link rel="stylesheet" href="css/reset/reset.css">
-    <!--[if lt IE 9]>
-      <script src="//html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
-    <![endif]-->
+    <title>Lol Endpoint</title>
+    <meta name="This is the Endpoint for lol engine" content="n">
+
   </head>
   <body>
+
+<label for="">Test Label</label>
 
   </body>
 </html>
